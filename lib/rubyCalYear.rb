b14870f1b544ff8f3349b0fd4100c9eb7452c073
Dane Hale <danehale0612@gmail.com>
2013-04-29 15:56:50 -0500

    added month class and made tests for month number to string and a test for number of days in a month

--- lib/rubyCalYear.rb
+++ lib/rubyCalYear.rb
@@ -1,12 +1,31 @@
-class Year
+class Calendar
+    attr_accessor = :year
+    attr_accessor = :month
 
+    def intialize
+        @month = Month.month
+        @year = Month.year
+    end
+
+    # def month_day_of_week
+    #     dayOfTheWeek = (month.daysInAMonth + ((@month +1)26)/10 +  )
+    # end
+
+# Year.year 
+end
+
+class Month
+
+    attr_accessor = :month 
     attr_accessor = :year
 
-    def initialize(year)
+
+    def initialize(month, year)
+        @month = month
         @year = year
     end
 
-    def leapYear?
+    def leap_year?
         if @year % 100 == 0 && @year % 400 == 0
              return true
         elsif @year % 4 == 0 && @year % 100 != 0
@@ -15,4 +34,18 @@ class Year
             return false
         end
     end
+
+    def string_month
+        monthToString = {1 => "January", 2 => "February", 3 => "March", 4 => "April", 5 => "May", 6 => "June", 7 => "July", 8 => "August", 9 => "September", 10 => "October", 11 => "November", 12 => "December"}
+        monthToString[@month]
+    end
+
+    def days_in_a_month
+        numOfDays = {1 => 31, 3 => 31, 5 => 31, 7 => 31, 9 => 31, 11 => 31, 4 => 30, 6 => 30, 8 => 30, 10 => 30, 12 => 30}
+        numOfDays[@month]
+    end
+
+    
+
+
 end
--- test/test_cal_integration.rb
+++ test/test_cal_integration.rb
@@ -8,8 +8,18 @@ class CalProjectTest < Test::Unit::TestCase
     end
 
     def test_2_is_this_a_leap_year?
-        year = Year.new(2100)
-        assert_equal(false, year.leapYear?)
+        year = Month.new(5, 2100)
+        assert_equal(false, year.leap_year?)
+    end
+
+    def test_3_number_month
+        month = Month.new(6, 2013)
+        assert_equal("June", month.string_month)
+    end
+
+    def test_4_num_of_days_in_month
+        calendar = Month.new(4, 2012)
+        assert_equal(30, calendar.days_in_a_month)
     end
 
 
